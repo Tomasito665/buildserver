3291b8e21f928bd5d3752a4d2f65572a85fc7a4a
Jordi Ortolá Ankum <jordi665@hotmail.com>
2017-12-03 23:47:30 +0100

    Move to cmake for portaudio build
    When building Mixxx in dynamic mode it gives unresolved external symbol errors (concerning functions defined in advapi32 and user32). Currently we only build the static portaudio libraries. However, in dynamic mode, the Mixxx scons script expects the dynamic portaudio library and thus advapi32 and user32 are not added (these are only needed for static PA), causing the unresolved external symbol errors. Portaudio's CMakeLists builds MSVC targets for both static and non-static versions of Portaudio. So, when we move to CMake, we can build static/dynamic builds depending on the mode given in build_environment.bat, which will get rid of the unresolved external symbol errors.

--- build_portaudio.bat
+++ build_portaudio.bat
@@ -1,12 +1,14 @@
 SETLOCAL
-echo "Building portaudio"
+echo ---- Building Portaudio ----
 set PORTAUDIO_PATH=pa_stable_v190600_20161030
-SET VALRETURN=0
+set VALRETURN=0
 
 if %MACHINE_X86% (
-  set PLATFORM=Win32
+  set CMAKE_GENERATOR="Visual Studio 14 2015"
+  set LIB_SUFFIX=x86
 ) else (
-  set PLATFORM=x64
+  set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
+  set LIB_SUFFIX=x64
 )
 
 if %CONFIG_RELEASE% (
@@ -15,17 +17,40 @@ if %CONFIG_RELEASE% (
   set CONFIG=Debug
 )
 
-cd build\%PORTAUDIO_PATH%\build\msvc
-%MSBUILD% portaudio.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:portaudio:Clean;portaudio:Rebuild
+if %STATIC_LIBS% (
+  set MSVC_TARGET=portaudio_static
+) else (
+  set MSVC_TARGET=portaudio
+)
+
+REM delete solution files
+cd build\%PORTAUDIO_PATH%\build
+rd /S /Q cmake
+mkdir cmake
+cd cmake
+
+REM generate solution
+"%CMAKEDIR%\cmake" ..\..\ -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%CONFIG% -DPA_USE_ASIO=ON
 IF ERRORLEVEL 1 (
-    SET VALRETURN=1
+    set VALRETURN=1
 	goto END
 )
 
-copy %PLATFORM%\%CONFIG%\portaudio.lib %LIB_DIR%
-if NOT %STATIC_LIBS% ( copy %PLATFORM%\%CONFIG%\portaudio.dll %LIB_DIR% )
-copy %PLATFORM%\%CONFIG%\portaudio.pdb %LIB_DIR%
+%MSBUILD% portaudio.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Portaudio\%MSVC_TARGET%:Clean;Portaudio\%MSVC_TARGET%:Rebuild
+IF ERRORLEVEL 1 (
+    set VALRETURN=1
+	goto END
+)
+
 copy ..\..\include\portaudio.h %INCLUDE_DIR%
+
+if %STATIC_LIBS% (
+  copy %CONFIG%\portaudio_static_%LIB_SUFFIX%.lib %LIB_DIR%
+) else (
+  copy %CONFIG%\portaudio_%LIB_SUFFIX%.exp %LIB_DIR%
+  copy %CONFIG%\portaudio_%LIB_SUFFIX%.lib %LIB_DIR%
+  copy %CONFIG%\portaudio_%LIB_SUFFIX%.dll %LIB_DIR%
+)
 
 :END
 cd %ROOT_DIR%
