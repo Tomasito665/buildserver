2fa17ed30dcf30196ba88de5525a17204d312c3b
Jordi Ortolá Ankum <jordi665@hotmail.com>
2017-12-05 01:22:13 +0100

    Build both static and dynamic versions of protobuf-lite
    Currently we only build the static version of protobuf, even when given Dynamic to build_environment.bat. This is problematic, because the Mixxx scons script expects the dynamic version of protobuf when static_libs is false and therefore adds the PROTOBUF_USE_DLLS preprocessor definition. This causes a bunch of unresolved external symbol "__declspec(dllimport) ... " errors when building Mixxx. Fix this by building the dynamic version of protobuf when the Dynamic flag is given.

--- build_protobuf.bat
+++ build_protobuf.bat
@@ -1,5 +1,5 @@
 SETLOCAL
-echo "Building protobuf"
+echo ---- Building protobuf ----
 set PROTOBUF_PATH=protobuf-2.6.1
 SET VALRETURN=0
 
@@ -16,7 +16,20 @@ if %CONFIG_RELEASE% (
 )
 
 cd build\%PROTOBUF_PATH%\vsprojects
-%MSBUILD% protobuf.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:libprotobuf-lite:Clean;libprotobuf-lite:Rebuild
+
+if %STATIC_LIBS% (
+  set MSVC_TARGET=libprotobuf-lite
+  set SOLUTION=protobuf.sln
+) else (
+  set MSVC_TARGET=libprotobuf-lite-dynamic
+  set SOLUTION=protobuf-patched.sln
+  REM create vc project to produce the dynamic version of libprotobuf-lite and
+  REM add it to a new protobuf-patched.sln solution
+  %BIN_DIR%\patch.exe libprotobuf-lite.vcxproj convert_libprotobuf-lite_to_dynamic.patch -o libprotobuf-lite-dynamic.vcxproj
+  %BIN_DIR%\patch.exe protobuf.sln add_libprotobuf-lite-dynamic_to_solution.patch -o protobuf-patched.sln
+)
+
+%MSBUILD% %SOLUTION% /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:%MSVC_TARGET%:Clean;%MSVC_TARGET%:Rebuild
 IF ERRORLEVEL 1 (
     SET VALRETURN=1
 	goto END
@@ -25,7 +38,7 @@ IF ERRORLEVEL 1 (
 if NOT %STATIC_LIBS% ( copy %PLATFORM%\%CONFIG%\libprotobuf-lite.dll %LIB_DIR% )
 copy %PLATFORM%\%CONFIG%\libprotobuf-lite.lib %LIB_DIR%
 copy %PLATFORM%\%CONFIG%\libprotobuf-lite.pdb %LIB_DIR%
-call extract_includes.bat 
+call extract_includes.bat
 xcopy /E /Y include %INCLUDE_DIR%
 
 :END
